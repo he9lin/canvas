5c339e49fc2317db3a6b8b59fd5d45ffa228d005
Lin He <he9lin@gmail.com>
2014-09-25 23:24:49 -0700

    Fix page test

--- test/models/page_test.exs
+++ test/models/page_test.exs
@@ -1,11 +1,17 @@
 defmodule CanvasTest.Page do
   use ExUnit.Case
+  require Logger
 
   test "can create many sections" do
-    page = %Page{name: "page 1"}
-    Repo.insert(page)
-    [page | _] = Repo.all(Page)
-    assert Enum.count(Repo.all(page.sections)) == 0
-    Repo.delete(page)
+    page     = Repo.insert(%Page{name: "page 1"})
+    sections = page.sections
+    assert Enum.count(Repo.all(sections)) == 0
+
+    Repo.insert(struct(sections, []))
+    assert Enum.count(Repo.all(page.sections)) == 1
+
+    # clean up
+    Repo.delete_all(Page)
+    Repo.delete_all(Section)
   end
 end
